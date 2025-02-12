206a6504bb47da34ad891144831c10a5979d8bf1
mpcooper23 <mpcooper23@gmail.com>
2025-02-12 16:02:15 -0600

    4

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -173,6 +173,11 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  //base case
+if(string.length === 0){
+  return
+}
+  //recursion
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
